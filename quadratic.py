a429982e3843befd2d063e6bb26699c3716f9a49
Timothee Blummer <59644997+TimoBlum@users.noreply.github.com>
2022-03-10 21:49:03 +0100

    updated
    now shows the tangent with the local slope to prove i understand the topics of my upcoming math exam

--- quadratic.py
+++ quadratic.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, math
 pygame.init()
 
 xy = 1000
@@ -7,6 +7,7 @@ pygame.display.set_caption("Quadratic Function Solver")
 clock = pygame.time.Clock()
 rows = 10
 spacebtwn = xy // rows
+started = False
 run = True
 
 
@@ -47,8 +48,40 @@ def drawGrid():
     pygame.draw.line(win, (30, 30, 30), (xy/2, 0), (xy/2, xy), width=2)
 
 
-def drawPoint(x, y):
-    pygame.draw.circle(win, (50, 255, 40), (x, y), 1)
+def euclidian(x1, y1, x2, y2):
+    return math.sqrt(((x2 - x1) ** 2) + (y2 - y1) ** 2)
+
+
+def clickedOn(place, radius):
+    pos = pygame.mouse.get_pos()
+    mouse = pygame.mouse.get_pressed(3)
+    if euclidian(pos[0], pos[1], place[0], place[1]) < radius and mouse[0]:
+        return True
+    else:
+        return False
+
+
+def drawPoint(x, y, width=1):
+    pygame.draw.circle(win, (50, 255, 40), (x, y), width)
+
+
+def drawLinear(m, point, color=(0, 0, 0)):
+    pygame.draw.line(win, color, (point[0] - 200, point[1] - m * 200), (point[0] + 200, point[1] + m * 200))
+
+
+def startingWin():
+    global started
+    if clickedOn((xy//2, xy//2), 150):
+        started = True
+    startImg = pygame.transform.scale(pygame.image.load("math background.jpg"), (xy, xy))
+    win.blit(startImg, (0, 0, 0, 0))
+    pygame.draw.circle(win, (0, 255, 0), (xy//2, xy//2), 150)
+    textOnScreen(xy//2, xy//2, 'Click to get started!', '', 30, color=(255, 0, 0))
+
+
+def showLocalTangent():
+    mousePos = pygame.mouse.get_pos()
+
 
 
 class Slider:
@@ -124,32 +157,42 @@ def func(a, b, c):
     while x < 600:
         dx = x
         dy = (0.01*a*((x+b*100)**2)+c*100)
+        localx = pygame.mouse.get_pos()[0]
+        localy = xy/2 - (0.01*a*(((localx-xy/2)+b*100)**2)+c*100)
+        drawPoint(localx, localy, width=10)
+        m = (2*a*(xy/2 - localx) / 100) + (2*a*-b)
+        drawLinear(m, (localx, localy))
         drawPoint(dx+xy/2, xy/2-dy)
         x += 0.1
 
 
 def redrawWin():
-    win.fill((255, 255, 255))
-    drawGrid()
-    textOnScreen(150, 50, "Mouse position is: ", str((pygame.mouse.get_pos()[0]/100)-5)[:4]+"  "+str(((pygame.mouse.get_pos()[1]/100)-5)*-1)[:4], 20)
-    aSlider.draw(), aSlider.interact()
-    bSlider.draw(), bSlider.interact()
-    cSlider.draw(), cSlider.interact()
-    a = 0.2+(aSlider.handleX-aSlider.handleXdef)/25
-    b = -4+(bSlider.handleX-bSlider.handleXdef)/25
-    c = -4+(cSlider.handleX-cSlider.handleXdef)/25
-    textOnScreen(xy-200, 50, "Quadratic function: ", str(a)[:4]+"(x + " + str(b)[:4] + ")2 + " + str(c)[:4], 20)
-    textOnScreen(aSlider.slideX+aSlider.slideRect.width/2, xy-50, "a value is: ", str(a)[:4], 20)
-    textOnScreen(bSlider.slideX+bSlider.slideRect.width/2, xy-50, "b value is: ", str(b)[:4], 20)
-    textOnScreen(cSlider.slideX+cSlider.slideRect.width/2, xy-50, "c value is: ", str(c)[:4], 20)
-    func(a, b, c)
-    pygame.display.update()
+    if started:
+        win.fill((255, 255, 255))
+        drawGrid()
+        textOnScreen(150, 50, "Mouse position is: ", str((pygame.mouse.get_pos()[0]/100)-5)[:4]+"  "+str(((pygame.mouse.get_pos()[1]/100)-5)*-1)[:4], 20)
+        aSlider.draw(), aSlider.interact()
+        bSlider.draw(), bSlider.interact()
+        cSlider.draw(), cSlider.interact()
+        a = 0.2+(aSlider.handleX-aSlider.handleXdef)/25
+        b = -4+(bSlider.handleX-bSlider.handleXdef)/25
+        c = -4+(cSlider.handleX-cSlider.handleXdef)/25
+        textOnScreen(xy-200, 50, "Quadratic function: ", str(a)[:4]+"(x + " + str(b)[:4] + ")2 + " + str(c)[:4], 20)
+        textOnScreen(aSlider.slideX+aSlider.slideRect.width/2, xy-50, "a value is: ", str(a)[:4], 20)
+        textOnScreen(bSlider.slideX+bSlider.slideRect.width/2, xy-50, "b value is: ", str(b)[:4], 20)
+        textOnScreen(cSlider.slideX+cSlider.slideRect.width/2, xy-50, "c value is: ", str(c)[:4], 20)
+        func(a, b, c)
+        print(a, b, c)
+        pygame.display.update()
+    else:
+        startingWin()
+        pygame.display.update()
 
 
 def main():
     global run
     while run:
-        clock.tick(100)
+        clock.tick(1000)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
